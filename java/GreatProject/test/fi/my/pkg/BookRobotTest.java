d5b6fd82cbb6b0edc386760b20d5f761d0f5411d
Ville <Ville.Hirvonen@gmail.com>
2022-03-08 19:30:19 +0200

    test

--- java/GreatProject/test/fi/my/pkg/BookRobotTest.java
+++ java/GreatProject/test/fi/my/pkg/BookRobotTest.java
@@ -2,8 +2,6 @@ package fi.my.pkg;
 
 import static org.mockito.Mockito.verify;
 
-import java.util.List;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
